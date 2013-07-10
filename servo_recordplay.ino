0c5513044b2dcd4086031136bc8a7c414deb9ad0
ladyada <limor@ladyada.net>
2013-07-10 16:43:24 -0400

    add url

--- servo_recordplay.ino
+++ servo_recordplay.ino
@@ -1,3 +1,8 @@
+// Example code for recording and playing back servo motion with a 
+// analog feedback servo
+// http://www.adafruit.com/products/1404
+
+
 #include <Servo.h>
 #include <EEPROM.h>
 
